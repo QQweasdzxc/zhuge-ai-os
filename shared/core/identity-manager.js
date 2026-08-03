49dd820b1dbe52e6b4f49937d38b31394c25f9a4
QQweasdzxc <35208075+QQweasdzxc@users.noreply.github.com>
2026-08-03 12:46:24 +0800

    ㄎㄠ

--- shared/core/identity-manager.js
+++
@@ -1,8 +0,0 @@
-/* Compatibility facade. New modules receive identity through ModuleContext. */
-(function (global) {
-  if (!global.ZhugeIdentity) throw new Error("ZhugeIdentity must load before IdentityManager.");
-  global.IdentityManager = Object.freeze({
-    normalize: source => global.ZhugeIdentity.normalize(source),
-    requireUserId: source => global.ZhugeIdentity.requireUserId(source)
-  });
-})(window);
