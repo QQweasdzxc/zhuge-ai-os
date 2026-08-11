2e6dcedbe0101e03dcc79e03ac516813618f165d
QQweasdzxc <35208075+QQweasdzxc@users.noreply.github.com>
2026-08-11 23:13:27 +0800

    del

--- shared/core/navigation-manager.js
+++
@@ -1,9 +0,0 @@
-/* Navigation contract. Modules receive routes from the root router and do not
- * mount a second Dashboard. */
-(function (global) {
-  function moduleRoute(module, path = "") {
-    return Object.freeze({ module: String(module || "dashboard"), path: String(path || "") });
-  }
-
-  global.NavigationManager = Object.freeze({ moduleRoute });
-})(window);
