d591ce59322cab622094600702c96e930a4811fd
QQweasdzxc <35208075+QQweasdzxc@users.noreply.github.com>
2026-09-05 08:49:18 +0800

    20260905-0845_Zhuge_AI_OS_Investment-A+C-Restore_PM-QA
    20260905-0845_Zhuge_AI_OS_Investment-A+C-Restore_PM-QA

--- shared/config/version.js
+++ shared/config/version.js
@@ -3,7 +3,7 @@
   const config = global.ZhugeFoundationConfig || {};
   config.version = Object.freeze({
     version: "0.9.0-alpha.9.13",
-    build: "20260905-0830",
+    build: "20260905-0845",
     foundation: "1.0",
     module: "Zhuge AI OS",
     moduleVersion: "0.9",
