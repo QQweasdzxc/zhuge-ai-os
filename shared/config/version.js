14df0b36abf010d13b072312b5bb0b0a70d0ddd4
QQweasdzxc <35208075+QQweasdzxc@users.noreply.github.com>
2026-09-18 16:39:42 +0800

    chore(release): start investment intelligence build 20260918-1638

--- shared/config/version.js
+++ shared/config/version.js
@@ -3,7 +3,7 @@
   const config = global.ZhugeFoundationConfig || {};
   config.version = Object.freeze({
     version: "0.9.0-alpha.9.13",
-    build: "20260917-1728",
+    build: "20260918-1638",
     foundation: "1.0",
     module: "Zhuge AI OS",
     moduleVersion: "0.9",
