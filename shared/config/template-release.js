b3300c73d8f7a84b874322e7105ab758de4bc82d
QQweasdzxc <35208075+QQweasdzxc@users.noreply.github.com>
2026-09-23 07:37:16 +0800

    build: advance runtime identity to 20260923-0736

--- shared/config/template-release.js
+++ shared/config/template-release.js
@@ -15,7 +15,7 @@
   "schemaVersion": 1,
   "templateId": "c",
   "developmentVersion": "0.9.0-alpha.9.13",
-  "developmentBuild": "20260922-1749",
+  "developmentBuild": "20260923-0736",
   "publishedVersion": "0.9.0-alpha.9.13",
   "publishedBuild": "20260915-1707",
   "templateVersion": "0.9.0-alpha.9.13",
