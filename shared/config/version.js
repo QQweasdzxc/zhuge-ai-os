3e64fa60e39acc2bddd49bb9fa7f301af3446e9e
QQweasdzxc <35208075+QQweasdzxc@users.noreply.github.com>
2026-09-26 00:42:54 +0800

    chore(release): advance candidate identity to 20260926-0039

--- shared/config/version.js
+++ shared/config/version.js
@@ -3,7 +3,7 @@
   const config = global.ZhugeFoundationConfig || {};
   config.version = Object.freeze({
     version: "0.9.0-alpha.9.13",
-    build: "20260926-0036",
+    build: "20260926-0039",
     foundation: "1.0",
     module: "Zhuge AI OS",
     moduleVersion: "0.9",
